e8daa1d9dcadef0594e8c2aeeff606eacc5c0929
unknown <gelargew@gmail.com>
2021-12-09 22:02:37 +0700

    horizontal

--- src/components/ScrollWrapper.tsx
+++ src/components/ScrollWrapper.tsx
@@ -1,26 +1,33 @@
 import React, { useContext, useEffect, useMemo, useRef } from 'react'
-import { useSpring, config, SpringConfig, animated, SpringValue, SpringRef } from '@react-spring/web'
+import { useSpring, config, SpringConfig, animated, SpringValue, SpringRef, to as interpolate } from '@react-spring/web'
 import { useGesture } from '@use-gesture/react'
 import { useResizeObserver, DOMRectProps } from '../utils/useResizeObserver'
 
 interface contextProps {
-    y: SpringValue<number>,
+    offset: {
+        x: SpringValue<number>;
+        y: SpringValue<number>;
+    },
     scrollTo: (page: number | string) => void,
     wrapper: DOMRectProps,
     content: DOMRectProps,
     height: number,
     spring: SpringRef<{
-        y: number;
+        y: number,
+        x: number
     }>
 }
 
 interface ScrollWrapperProps extends React.HTMLAttributes<HTMLDivElement> {
     springConfig?: SpringConfig,
     scrollSpeed?: number,
-    innerProps: React.HTMLAttributes<HTMLDivElement>
+    innerProps: React.HTMLAttributes<HTMLDivElement>,
+    horizontal?: boolean
 }
 
 const ScrollContext = React.createContext({} as contextProps)
+
+const transf = (x: number, y: number) => `translate3d(${x}, ${y}px, 0)`
 
 const ScrollWrapper = ({
     springConfig = config.molasses,
@@ -28,34 +35,55 @@ const ScrollWrapper = ({
     children,
     style,
     innerProps,
+    horizontal = false,
     ...props
 }: ScrollWrapperProps) => {
     const ref = useRef<HTMLDivElement>(null)
     const wrapperRef = useRef<HTMLDivElement>(null)
     const wrapper = useResizeObserver(wrapperRef)
     const content = useResizeObserver(ref)
-    const height = useMemo(() => content.height - wrapper.height, [wrapper, content])
+    const [height, width] = useMemo(() => {
+        return [content.height - wrapper.height, content.width - wrapper.width]
+    }, [wrapper, content])
 
-    const [{y}, spring] = useSpring(() => ({
+    const [offset, spring] = useSpring(() => ({
         y: 0,
+        x: 0,
         config: springConfig
     }))
 
     //update Y value according to deltaY in pixel value
     const updateY = (deltaY: number) => {
-        let target = y.animation.to as number + (deltaY * scrollSpeed)
-        if (target > 1) {
-            target = 0
-        }
-        else if (target < -height) {
-            target = -height
-        }
-        spring.start({y: target})
+        let target = offset.y.animation.to as number + (deltaY * scrollSpeed)
+        if (target > 1) target = 0
+        else if (target < -height) target = -height  
+        spring.start({x: offset.x, y: target})
+    }
+
+    const updateX = (deltaX: number) => {
+        let target = offset.x.animation.to as number + (deltaX * scrollSpeed)
+        if (target > 1) target = 0
+        else if (target < -width) target = -width
+        spring.start({x: target, y: offset.y})
     }
 
     const bind = useGesture({
-        onWheel: state => state.active && updateY(-state.movement[1]/2),
-        onDrag: state => updateY(state.delta[1] * 2.5 * state.velocity[1])
+        onWheel: state => {
+            if (horizontal) {
+                state.active && updateX(-state.movement[0]/2)
+            }
+            else {
+                state.active && updateY(-state.movement[1]/2)
+            }
+        },
+        onDrag: state => {
+            if (horizontal) {
+                updateX(state.delta[0] * 2.5 * state.velocity[0])
+            }
+            else {
+                updateY(state.delta[1] * 2.5 * state.velocity[1])
+            }
+        }
     })
 
     //update Y to any pixel value or to any direct children given its index
@@ -73,7 +101,8 @@ const ScrollWrapper = ({
 
     useEffect(() => {
         updateY(0)
-    }, [height])
+        updateX(0)
+    }, [height, width])
 
     return (
         <div
@@ -90,12 +119,12 @@ const ScrollWrapper = ({
             <animated.div 
             ref={ref} 
             style={{
-                transform: y.to(y => `translate3d(0, ${y}px, 0)`)
+                transform: interpolate([offset.x, offset.y], transf)
             }}
             {...innerProps}
             >
                 <ScrollContext.Provider value={{
-                    y,
+                    offset,
                     scrollTo,
                     wrapper,
                     content,
